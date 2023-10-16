d2f59636dec83357cdecc5ba32e35d2556dc3445
Nickolas Ribeiro <nickolasleosantos@hotmail.com>
2023-10-16 13:31:56 -0300

    feat(section): routes

--- src/api/modules/index.ts
+++ src/api/modules/index.ts
@@ -1,12 +1,15 @@
 import {Router} from "express";
 import { PrismaClient } from '@prisma/client'
 import { UserModule } from "./user/module/user-model";
+import { SectionModule } from "./section/module/section-module";
 
 const prismaClient = new PrismaClient();
 const router: Router = Router();
 const userModule = new UserModule(prismaClient);
+const sectionModule = new SectionModule(prismaClient);
 
 //Exemplo 
 router.use('/user', userModule.userRouter.router);
+router.use('/section', sectionModule.sectionRouter.router);
 
 export {router}
---
+++ src/api/modules/section/controller/section-controller.ts
@@ -0,0 +1,72 @@
+import { PrismaClient } from '@prisma/client'
+import { Request, Response } from 'express'
+import SectionService from '../service/section-service';
+
+export default class SectionController {
+  private readonly service: SectionService 
+
+  constructor(public prismaClient: PrismaClient) {
+    this.service = new SectionService(prismaClient)
+  }
+
+  //fetch all sections
+  public async getAll(request: Request, response: Response){
+    try{
+      const sections = await this.service.getAll();
+      
+      return response.json(sections).status(200);
+    } catch (error: any) {
+      return response.json({
+        error: error.message
+      }).status(500)
+    }
+  }
+
+  public async saveSection(request: Request, response: Response) {
+    try {
+      const user = await this.service.saveSection(request.body)
+      
+      return response.json({
+        user
+      }).status(200);
+
+    } catch (error: any) {
+      return response.json({
+        error: error.message
+      }).status(500)
+    }
+  }
+
+  public async updateSection(request: Request, response: Response){
+    try {
+      const sectionId = parseInt(request.params.id)
+      let payload = request.body;
+
+      const section = await this.service.updateSection(sectionId, payload)
+      
+      return response.json({
+        section
+      }).status(200);
+    } catch (error: any) {
+      return response.json({
+        error: error.message
+      }).status(500)
+    }
+  }
+
+  public async deleteSection(request: Request, response: Response) {
+    try {
+      const sectionId = parseInt(request.params.id)
+      const section = await this.service.deleteSection(sectionId)
+
+      return response.json({
+        section
+      }).status(200);
+    } catch (error: any) {
+      return response.json({
+        error: error.message
+      }).status(500)
+    }
+  }
+
+}
